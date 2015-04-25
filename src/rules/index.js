255872d4a66c0c548ca02b829be6d31628746fe1
Nate-Wilkins <nwilkins2012@gmail.com>
2015-04-25 12:46:08 -0400

    fix(plugin): update interface to eslint 0.8.0

--- src/rules/index.js
+++ src/rules/index.js
@@ -1,6 +1,6 @@
 /**
  * @object
- * @version 1.0.0
+ * @version 1.0.4
  * @author nate-wilkins <nwilkins2012@gmail.com> (https://github.com/nate-wilkins)
  * @license MIT (c) 2014
  *
@@ -10,4 +10,11 @@
 'use strict';
 
 var requireDirectory = require('require-directory');
-module.exports = requireDirectory(module);
+
+module.exports = {
+	rules: requireDirectory(module),
+	rulesConfig: {
+		'exclude': 2,
+		'focus': 2
+	}
+};
